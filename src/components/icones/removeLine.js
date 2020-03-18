aed20c92b70549b383d16f25ba4be7a74f345206
davi.ribeiro <davi.ribeiro@ibti.org.br>
2020-03-18 12:30:07 -0300

    change icon

--- src/components/icones/removeLine.js
+++ src/components/icones/removeLine.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 export function IcoRemoveLine(props){
 	return (
-		<svg className="icon" viewBox="0 0 100 100" style={props.style?props.style:{}}>
+		<svg className="icon" viewBox="0 0 32 32" style={props.style?props.style:{}}>
 			<title>{props.title?props.title:null}</title>
-			<rect x="13.29" y="6.61" width="73.42" height="4.38"/>
-			<polygon points="59.96 3 40.04 3 36.19 6.61 63.81 6.61 59.96 3"/>
-			<path d="M71.8,98H28.2a7.31,7.31,0,0,1-7.3-6.88L16.5,16.41h67L79.1,91.12A7.31,7.31,0,0,1,71.8,98ZM20.74,20.4l4.14,70.48A3.34,3.34,0,0,0,28.2,94H71.8a3.34,3.34,0,0,0,3.32-3.13L79.26,20.4Z"/>
-			<rect x="20.86" y="55.78" width="77.67" height="2.85" transform="translate(-0.06 114.34) rotate(-87.49)"/>
-			<rect x="38.87" y="18.37" width="2.85" height="77.67" transform="translate(-2.48 1.83) rotate(-2.53)"/>
+			<path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+			<path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
 		</svg>
 	);
 	
